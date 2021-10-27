104f59218f2c6fb61426fafeef8ded0327be15c6
marwan darwish <darawish663@gmail.com>
2021-10-27 19:54:22 +0200

    ay 7aga

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ app.use(express.json({extended:false}))
 app.use("/api/users",require("./routes/users"))
 app.use("/api/contacts",require("./routes/contacts"))
 app.use("/api/auth",require("./routes/auth"))
-if(process.env.Node_ENV==="production"){
+if(process.env.NODE_ENV==="production"){
     app.use(express.static("client/build"))
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,"client","build","index.html"))
-    })
+    app.get("*",(req,res)=>
+        res.sendFile(path.resolve(__dirname,"client", "build", "index.html"))
+    )
 }
-const PORT=process.env.PORT ||7000
+const PORT=process.env.PORT ||8888
 
 app.listen(PORT,()=>console.log("server started"))
